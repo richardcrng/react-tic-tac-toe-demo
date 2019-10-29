23df6599ad00d48b614528fa87a05b36473dffe3
richardcrng@gmail.com <richardcrng@gmail.com>
2019-10-29 09:51:13 +0000

    Pass board content to Cell via content prop

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Cell from './Cell'
 
 function Board() {
+  const boardContent = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
+
   return (
     <div>
-      Board
+      <div>
+        <Cell content={boardContent[0][0]} />
+        <Cell content={boardContent[0][1]} />
+        <Cell content={boardContent[0][2]} />
+      </div>
+      <div>
+        <Cell content={boardContent[1][0]} />
+        <Cell content={boardContent[1][1]} />
+        <Cell content={boardContent[1][2]} />
+      </div>
+      <div>
+        <Cell content={boardContent[2][0]} />
+        <Cell content={boardContent[2][1]} />
+        <Cell content={boardContent[2][2]} />
+      </div>
     </div>
   )
 }
