ebb8529bb261226f20e886bdd070d57e849798a2
richardcrng@gmail.com <richardcrng@gmail.com>
2019-10-29 10:00:49 +0000

    Use grid layout in Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,27 +4,25 @@ import Cell from './Cell'
 function Board() {
   const boardContent = [
     ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    ['', 'X', ''],
+    ['', 'O', 'X']
   ]
 
   return (
-    <div>
-      <div>
-        <Cell content={boardContent[0][0]} />
-        <Cell content={boardContent[0][1]} />
-        <Cell content={boardContent[0][2]} />
-      </div>
-      <div>
-        <Cell content={boardContent[1][0]} />
-        <Cell content={boardContent[1][1]} />
-        <Cell content={boardContent[1][2]} />
-      </div>
-      <div>
-        <Cell content={boardContent[2][0]} />
-        <Cell content={boardContent[2][1]} />
-        <Cell content={boardContent[2][2]} />
-      </div>
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(3, 100px)',
+      gridTemplateRows: 'repeat(3, 100px)'
+    }}>
+      <Cell content={boardContent[0][0]} />
+      <Cell content={boardContent[0][1]} />
+      <Cell content={boardContent[0][2]} />
+      <Cell content={boardContent[1][0]} />
+      <Cell content={boardContent[1][1]} />
+      <Cell content={boardContent[1][2]} />
+      <Cell content={boardContent[2][0]} />
+      <Cell content={boardContent[2][1]} />
+      <Cell content={boardContent[2][2]} />
     </div>
   )
 }
