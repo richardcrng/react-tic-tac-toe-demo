3b10d80257205bbc487157e06484caa7cd0e0bc0
richardcrng@gmail.com <richardcrng@gmail.com>
2019-10-29 10:21:57 +0000

    Demonstrate update function for boardContent

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,21 +9,39 @@ function Board() {
   ])
 
   return (
-    <div style={{
-      display: 'grid',
-      gridTemplateColumns: 'repeat(3, 100px)',
-      gridTemplateRows: 'repeat(3, 100px)'
-    }}>
-      <Cell content={boardContent[0][0]} />
-      <Cell content={boardContent[0][1]} />
-      <Cell content={boardContent[0][2]} />
-      <Cell content={boardContent[1][0]} />
-      <Cell content={boardContent[1][1]} />
-      <Cell content={boardContent[1][2]} />
-      <Cell content={boardContent[2][0]} />
-      <Cell content={boardContent[2][1]} />
-      <Cell content={boardContent[2][2]} />
-    </div>
+    <>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 100px)',
+        gridTemplateRows: 'repeat(3, 100px)'
+      }}>
+        <Cell content={boardContent[0][0]} />
+        <Cell content={boardContent[0][1]} />
+        <Cell content={boardContent[0][2]} />
+        <Cell content={boardContent[1][0]} />
+        <Cell content={boardContent[1][1]} />
+        <Cell content={boardContent[1][2]} />
+        <Cell content={boardContent[2][0]} />
+        <Cell content={boardContent[2][1]} />
+        <Cell content={boardContent[2][2]} />
+      </div>
+      <div
+        onClick={() => {
+          // assume we're updating row index 0 and column index 0
+          const copyOfBoard = [
+            [...boardContent[0]],
+            [...boardContent[1]],
+            [...boardContent[2]]
+          ]
+
+          copyOfBoard[0][0] = 'X'
+
+          setBoardContent(copyOfBoard)
+        }}
+      >
+        Demonstration of update function
+      </div>
+    </>
   )
 }
 
